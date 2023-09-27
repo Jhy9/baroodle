b7121051fd3ca8de08cd20df89a0d7aa5aac00c6
Juuso Hyyryläinen <juusohyyrylainen@hotmail.com>
2023-09-27 09:57:53 +0300

    Login fixed

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template,request
-import user
+import users
 
 @app.route("/")
 def index():
@@ -14,8 +14,8 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
         account_type = request.form["account_type"]
-        if user.register(username,password,account_type):
-            id = user.login(username,password)
+        if users.register(username,password,account_type):
+            id = users.login(username,password)
             return render_template("main-page.html", id = id)
         else:
             return render_template("register-error.html")
@@ -27,7 +27,7 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        id = user.login(username,password)
+        id = users.login(username,password)
         if id is False:
             return render_template("login-error.html")            
         else:            
--- users.py
+++ users.py
@@ -1,8 +1,9 @@
 from db import db
 from flask import session
+from sqlalchemy import text
 def register(username, password, account_type):
     try:
-        sql = "INSERT INTO accounts(username,password, account_type) VALUES (:username,:password,:account_type)"
+        sql = text('INSERT INTO accounts(username,passw, account_type) VALUES (:username,:password,:account_type)')
         db.session.execute(sql, {"username":username,"password":password,"account_type":account_type})
         db.session.commit()
     except:
@@ -10,12 +11,13 @@ def register(username, password, account_type):
     return True
 
 def login(username, password):
-    sql = "SELECT id, password FROM accounts WHERE username=:username"
-    result = db.session.execute(sql, {"username":username})
+    sql = text('SELECT id, passw FROM accounts WHERE username=:username')
+    query = db.session.execute(sql, {"username":username})
+    result = query.fetchone()
     if not result:
         return False
     else:
-        if result.password ==password:
+        if result.passw ==password:
             return result.id
         else:
             return False
